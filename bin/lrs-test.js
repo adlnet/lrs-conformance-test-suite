7ce4f9ec05107aced1b59778fa4a6b9fc24ebfeb
Fredrick Whorton <fred.whorton@riptidesoftware.com>
2015-05-08 14:47:11 -0400

    Update selecting test folder using directory.  Cleanup / refactor documents.js.  Update version.

--- bin/lrs-test.js
+++ bin/lrs-test.js
@@ -6,10 +6,12 @@
 (function (process, require, program, exit, packageJson, Q, Joi, fs, path, Mocha) {
     'use strict';
 
+    var DIRECTORY = 'v1_0_2';
+
     program
         .version(packageJson.version)
         .usage('[options]')
-        .option('-d, --directory [path]', 'test directory, default: test/v1_0_2')
+        .option('-d, --directory [path]', 'test directory, default: ' + DIRECTORY)
         .option('-e, --endpoint <path>', 'the connection string')
         .option('-a, --basicAuth <true/false>', 'enables basic authentication')
         .option('-u, --authUser <username>', 'sets user name (required when basic authentication enabled)')
@@ -33,7 +35,7 @@
 
     process.nextTick(function () {
         var options = {
-                directory: program.directory || 'test/v1_0_2',
+                directory: program.directory || DIRECTORY,
                 endpoint: program.endpoint,
                 basicAuth: program.basicAuth,
                 authUser: program.authUser,
@@ -48,7 +50,7 @@
             process.env.BASIC_AUTH_ENABLED = options.basicAuth;
             process.env.BASIC_AUTH_USER = options.authUser;
             process.env.BASIC_AUTH_PASSWORD = options.authPass;
-            var testDirectory = options.directory;
+            var testDirectory = 'test/' + options.directory;
             fs.readdirSync(testDirectory).filter(function (file) {
                 return file.substr(-3) === '.js';
             }).forEach(function (file) {
