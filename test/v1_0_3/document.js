ee7579639601efe508e12f1fc86f3e64c952a95d
Lou Wolford <lou.wolford.ctr@adlnet.gov>
2016-09-23 09:43:11 -0400

    fixed indentation

--- test/v1_0_3/document.js
+++ test/v1_0_3/document.js
@@ -1341,7 +1341,7 @@
                 .then(function (res) {
                     var about = res.body;
                     expect(about).to.have.property('version').to.be.an('array');
-                   var validVersions = ['0.9', '0.95', '.9', '.95', '1.0.0', '1.0.1', '1.0.2', '1.0.3'];
+                    var validVersions = ['0.9', '0.95', '.9', '.95', '1.0.0', '1.0.1', '1.0.2', '1.0.3'];
                     about.version.forEach(function (item) {
                         expect(validVersions).to.include(item);
                     })
