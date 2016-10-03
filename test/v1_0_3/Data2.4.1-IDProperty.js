dd5988477d5019cdadea206b55fa11023b5c6e5e
Andy Creighton <cr8onski@gmail.com>
2016-10-03 10:37:40 -0400

    enclosed loose it in a describe

--- test/v1_0_3/Data2.4.1-IDProperty.js
+++ test/v1_0_3/Data2.4.1-IDProperty.js
@@ -17,33 +17,36 @@ describe('Id Property Requirements (Data 2.4.1)', () => {
 
     templatingSelection.createTemplate('uuids.js');
 
-    it ('An LRS generates the "id" property of a Statement if none is provided (Modify, Data 2.4.1.s2.b1)', function (done){
-        this.timeout(0);
-        var templates = [
-            {statement: '{{statements.default}}'}
-        ];
-        data = helper.createFromTemplate(templates);
-        data = data.statement;
-        var stmtTime = Date.now();
+    describe ('An LRS generates the "id" property of a Statement if none is provided (Modify, Data 2.4.1.s2.b1)', function (){
 
-        request(helper.getEndpointAndAuth())
-        .post(helper.getEndpointStatements())
-        .headers(helper.addAllHeaders({}))
-        .json(data)
-        .expect(200)
-        .end()
-        .get(helper.getEndpointStatements() + '?limit=1')
-        .wait(helper.genDelay(stmtTime, null, null))
-        .headers(helper.addAllHeaders({}))
-        .end(function (err, res) {
-            if (err) {
-                done(err);
-            } else {
-                var results = helper.parse(res.body, done);
-                expect(results.statements[0].id).to.not.be.undefined;
-                done();
-            }
+        it('should complete an empty id property', (done) => {
+            this.timeout(0);
+            var templates = [
+                {statement: '{{statements.default}}'}
+            ];
+            data = helper.createFromTemplate(templates);
+            data = data.statement;
+            var stmtTime = Date.now();
+
+            request(helper.getEndpointAndAuth())
+            .post(helper.getEndpointStatements())
+            .headers(helper.addAllHeaders({}))
+            .json(data)
+            .expect(200)
+            .end()
+            .get(helper.getEndpointStatements() + '?limit=1')
+            .wait(helper.genDelay(stmtTime, null, null))
+            .headers(helper.addAllHeaders({}))
+            .end(function (err, res) {
+                if (err) {
+                    done(err);
+                } else {
+                    var results = helper.parse(res.body, done);
+                    expect(results.statements[0].id).to.not.be.undefined;
+                    done();
+                }
             });
+        });
     });
 
 });
