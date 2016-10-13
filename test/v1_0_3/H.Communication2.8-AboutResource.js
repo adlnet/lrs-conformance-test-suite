071943761292d34fc21f85dfa5ef26df24230089
Andy Creighton <cr8onski@gmail.com>
2016-10-13 14:08:46 -0400

    matchup comments comm 2.8 about profile resource and comm 3.1 concurrency

--- test/v1_0_3/H.Communication2.8-AboutResource.js
+++ test/v1_0_3/H.Communication2.8-AboutResource.js
@@ -15,14 +15,31 @@
 
 describe('About Resource Requirements (Communication 2.8)', () => {
 
-    it('An LRS has an About Resource with endpoint "base IRI"+"/about" (Communication 2.8)', function () {
+/**  Matchup with Conformance Requirements Document
+ * XAPI-00315 - below
+ * XAPI-00316 - below
+ * XAPI-00317 - below
+ * XAPI-00318 - below
+ * XAPI-00319 - below
+ * XAPI-00320 - below
+ * XAPI-00321 - not found yet - An LRS rejects with error code 400 Bad Request, a Request which does not use a "X-Experience-API-Version" header name to any API except the About API
+ */
+
+/**  XAPI-00315, Communication 2.8 About Resource
+ * An LRS has an About API with endpoint "base IRI"+"/about"
+ */
+    it('An LRS has an About Resource with endpoint "base IRI"+"/about" (Communication 2.8, XAPI-00315)', function () {
         return helper.sendRequest('get', '/about', undefined, undefined, 200);
     });
 
-    it('An LRS\'s About Resource accepts GET requests (Communication 2.8)', function () {
+/**  XAPI-00319, Communication 2.8 About Resource
+ * An LRS's About Resource accepts GET requests. Upon processing a successful GET request returns a version property and code 200 OK
+ */
+    it('An LRS\'s About Resource accepts GET requests (Communication 2.8, XAPI-00319)', function () {
         return helper.sendRequest('get', '/about', undefined, undefined, 200);
     });
 
+    //see above
     it('An LRS\'s About Resource upon processing a successful GET request returns a version property and code 200 OK (multiplicity, Communication 2.8.s4)', function () {
         return helper.sendRequest('get', '/about', undefined, undefined, 200)
             .then(function (res) {
@@ -31,7 +48,10 @@ describe('About Resource Requirements (Communication 2.8)', () => {
             });
     });
 
-    it('An LRS\'s About Resource\'s version property is an array of strings (format, Communication 2.8.s4.table1.row1)', function () {
+/**  XAPI-00318, Communication 2.8 About Resource
+ * An LRS's About API's version property is an array of strings
+ */
+    it('An LRS\'s About Resource\'s version property is an array of strings (format, Communication 2.8.s4.table1.row1, XAPI-00318)', function () {
         return helper.sendRequest('get', '/about', undefined, undefined, 200)
             .then(function (res) {
                 var about = res.body;
@@ -39,8 +59,11 @@ describe('About Resource Requirements (Communication 2.8)', () => {
             });
     });
 
+/**  XAPI-00317, Communication 2.8 About Resource
+ * An LRS's About API's version property contains at least one string of "1.0.x"
+ */
     //moving this over for now, string value will need updated
-    it('An LRS\'s About Resource\'s version property contains at least one string of "1.0.1" (Communication 2.8.s5.b1.b1)', function () {
+    it('An LRS\'s About Resource\'s version property contains at least one string of "1.0.1" (Communication 2.8.s5.b1.b1, XAPI-00317)', function () {
         return helper.sendRequest('get', '/about', undefined, undefined, 200)
             .then(function (res) {
                 var about = res.body;
@@ -56,7 +79,10 @@ describe('About Resource Requirements (Communication 2.8)', () => {
             });
     });
 
-    it('An LRS\'s About Resource\'s version property can only have values of "0.9", "0.95", "1.0.0", or ""1.0." + X" with (Communication 2.8.s5.b1.b1)', function () {
+/**  XAPI-00316, Communication 2.8 About Resource
+ * An LRS's About API's version property can only have values of "0.9", "0.95", "1.0.0", or “1.0.x” with
+ */
+    it('An LRS\'s About Resource\'s version property can only have values of "0.9", "0.95", "1.0.0", or ""1.0." + X" with (Communication 2.8.s5.b1.b1, XAPI-00316)', function () {
         return helper.sendRequest('get', '/about', undefined, undefined, 200)
             .then(function (res) {
                 var about = res.body;
@@ -68,8 +94,11 @@ describe('About Resource Requirements (Communication 2.8)', () => {
             });
     });
 
+/**  XAPI-00320, Communication 2.8 About Resource
+ * An LRS's About API upon processing a successful GET request can return an Extension Object with code 200 OK
+ */
 // This is a bad test.  Extensions property is option in the spec.
-//     it('An LRS\'s About Resource upon processing a successful GET request can return an Extension with code 200 OK (multiplicity, Communication 2.8.s4.table1.row2)', function () {
+//     it('An LRS\'s About Resource upon processing a successful GET request can return an Extension with code 200 OK (multiplicity, Communication 2.8.s4.table1.row2, XAPI-00320)', function () {
 //         return helper.sendRequest('get', helper.getEndpointAbout(), undefined, undefined, 200)
 //             .then(function (res) {
 //                 var about = res.body;
