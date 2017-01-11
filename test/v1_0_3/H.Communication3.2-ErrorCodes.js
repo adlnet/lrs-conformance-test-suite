13130a60e3e22a889af124f732f62329e907ddc8
Andy Creighton <cr8onski@gmail.com>
2017-01-11 10:11:28 -0500

    fixing broken link - remove duplicate test in comm 3.2, covered by comm 4.0 xapi-00334 & 00335

--- test/v1_0_3/H.Communication3.2-ErrorCodes.js
+++ test/v1_0_3/H.Communication3.2-ErrorCodes.js
@@ -32,17 +32,6 @@ describe('Error Codes Requirements (Communication 3.2)', () => {
             .expect(400, done)
     });
 
-    if(!global.OAUTH)
-    {
-        //This test appears to only make sense in the case of http basic Auth. Should we have additional tests for bad OAUTH, which is more complicated?
-        it('An LRS rejects a Statement of bad authorization (either authentication needed or failed credentials) with error code 401 Unauthorized (Communication 3.2.s2.b2)', function (done) {
-            request(helper.getEndpointAndAuth())
-                .get(helper.getEndpointStatements())
-                .headers(helper.addAllHeaders({}, true))
-                .expect(401, done);
-        });
-    }
-
     describe('An LRS rejects with error code 405 Method Not Allowed to any request to an Resource which uses a method not in this specification **Implicit ONLY in that HTML normally does this behavior** (Communication 3.2.s3.b1)', function () {
 
         it('should fail with statement "DELETE"', function (done) {
