a074f236d812ddb4db171ba8a508da0e37a0df48
Andy Creighton <cr8onski@gmail.com>
2017-01-30 16:31:31 -0500

    update req for data 4.0

--- test/v1_0_3/configs/verify.js
+++ test/v1_0_3/configs/verify.js
@@ -846,8 +846,66 @@
                         expect: [200]
                     }
                 ]
+            },
+            {
+                name: 'Languages Verify Templates',
+                config: [
+                    {
+                        name: 'should pass statement verb template',
+                        templates: [
+                            {statement: '{{statements.verb}}'},
+                            {verb: '{{verbs.no_display}}'}
+                        ],
+                        expect: [200]
+                    },
+                    {
+                        name: 'should pass statement object template',
+                        templates: [
+                            {statement: '{{statements.object_activity}}'},
+                            {object: '{{activities.no_languages}}'}
+                        ],
+                        expect: [200]
+                    },
+                    {
+                        name: 'should pass statement attachment template',
+                        templates: [
+                            {statement: '{{statements.attachment}}'},
+                            {
+                                attachments: [
+                                    {
+                                        "usageType": "http://example.com/attachment-usage/test",
+                                        "display": {"en-US": "A test attachment"},
+                                        "description": {"en-US": "A test attachment (description)"},
+                                        "contentType": "text/plain; charset=ascii",
+                                        "length": 27,
+                                        "sha2": "495395e777cd98da653df9615d09c0fd6bb2f8d4788394cd53c56a3bfdcd848a",
+                                        "fileUrl": "http://over.there.com/file.txt"
+                                    }
+                                ]
+                            }
+                        ],
+                        expect: [200]
+                    },
+                    {
+                        name: 'should pass statement substatement verb template',
+                        templates: [
+                            {statement: '{{statements.object_substatement}}'},
+                            {object: '{{substatements.verb}}'},
+                            {verb: '{{verbs.no_display}}'}
+                        ],
+                        expect: [200]
+                    },
+                    {
+                        name: 'should pass statement substatement object template',
+                        templates: [
+                            {statement: '{{statements.object_substatement}}'},
+                            {object: '{{substatements.activity}}'},
+                            {object: '{{activities.no_languages}}'}
+                        ],
+                        expect: [200]
+                    }
+                ]
             }
-
         ];
     };
 }(module));
