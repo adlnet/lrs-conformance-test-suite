e0ff398a751d2fc572e2141660ec4a2d627cde01
Argenrost <happycasanova@gmail.com>
2021-04-01 16:38:51 -0400

    Tests for context agents. Template for groups.

---
+++ test/v2_0/configs/contextagents.js
@@ -0,0 +1,113 @@
+/**
+ * Description : This is a test suite that tests an LRS endpoint based on the testing requirements document
+ * found at https://github.com/adlnet/xAPI_LRS_Test/blob/master/TestingRequirements.md
+ *
+ * https://github.com/adlnet/xAPI_LRS_Test/blob/master/TestingRequirements.md
+ *
+ */
+(function (module) {
+    "use strict";
+
+    // Defines overwriting data.
+    var INVALID_OBJECT_TYPE = {
+        contextAgents: [
+            {
+                objectType: "Should fail",
+                agent: {
+                    objectType: "Agent",
+                    mbox: "mailto:player-1@example.com"
+                }
+            }
+        ]
+    }
+    var INVALID_AGENT = {
+        contextAgents: [
+            {
+                objectType: "contextAgent",
+                agent: {
+                    key: "Should fail"
+                }
+            }
+        ]
+    }
+    var INVALID_OBJECT = { key: 'should fail' };
+    var INVALID_STRING = 'should fail';
+    var VALID_ACTIVITY = {
+        "objectType": "Activity",
+        "id": "http://www.example.com/meetings/occurances/34534"
+    };
+
+    // Configures tests.
+    module.exports.config = function () {
+        return [
+            {
+                /** ContextAgents Property
+                 *  A "contextAgents" property is an array of "contextAgent" Objects.
+                 */
+                name: 'A "contextAgents" property is an array of "contextAgent" Objects',
+                config: [
+                    {
+                        name: 'Statement with "contextAgents" property has an array of valid "contextAgent" Objects',
+                        templates: [
+                            { statement: '{{statements.context}}' },
+                            { context: '{{contexts.context_agents}}' }
+                        ],
+                        expect: [200]
+                    },
+                    {
+                        name: 'Statement substatement with "contextAgents" property has an array of valid "contextAgent" Objects',
+                        templates: [
+                            { statement: '{{statements.object_substatement}}' },
+                            { object: '{{substatements.context}}' },
+                            { context: '{{contexts.context_agents}}' }
+                        ],
+                        expect: [200]
+                    }
+                ]
+            },
+            {
+                /** ContextAgents Object
+                 *  A "contextAgents" Object must have an "objectType" property of string "contextAgent" and a valid Agent Object "agent";
+                 *  it may also have a "relevantTypes" property as an array of Activity Type IRIs.
+                 */
+                name: 'A "contextAgents" Object must have an "objectType" property of string "contextAgent" and a valid Agent Object "agent"',
+                config: [
+                    {
+                        name: 'Statement with "contextAgents" Object rejects statement if "objectType" property is anything other than string "contextAgent"',
+                        templates: [
+                            { statement: '{{statements.context}}' },
+                            { context: INVALID_OBJECT_TYPE }
+                        ],
+                        expect: [400]
+                    },
+                    {
+                        name: 'Statement with "contextAgents" Object rejects statement if "agent" property is invalid',
+                        templates: [
+                            { statement: '{{statements.context}}' },
+                            { context: INVALID_AGENT }
+                        ],
+                        expect: [400]
+                    },
+                    {
+                        name: 'Statement substatement with "contextAgents" Object rejects statement if "objectType" property is anything other than string "contextAgent"',
+                        templates: [
+                            { statement: '{{statements.object_substatement}}' },
+                            { object: '{{substatements.context}}' },
+                            { context: INVALID_OBJECT_TYPE }
+                        ],
+                        expect: [400]
+                    },
+                    {
+                        name: 'Statement substatement with "contextAgents" Object rejects statement if "agent" property is invalid',
+                        templates: [
+                            { statement: '{{statements.object_substatement}}' },
+                            { object: '{{substatements.context}}' },
+                            { context: INVALID_AGENT }
+                        ],
+                        expect: [400]
+                    }
+                ]
+            }
+        ];
+    };
+}(module));
