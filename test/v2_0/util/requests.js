108b9198740f764ab9fba285f5198e635751da3e
TreyH <vbhayden3@gmail.com>
2023-05-09 13:14:55 -0400

    adding activity state + agent profile to new lib

--- test/v2_0/util/requests.js
+++ test/v2_0/util/requests.js
@@ -2,6 +2,7 @@ const path = require("path");
 const axios = require("axios").default;
 const addOAuthInterceptor = require("axios-oauth-1.0a").default;
 const chai = require("chai");
+const oldHelpers = require("../../helper");
 
 /** Test directory */
 const DIRECTORY = process.env.DIRECTORY;
@@ -75,7 +76,7 @@ const requests = {
         });
     },
 
-    getActivityWithIRI: async(iri) => {
+    getActivityWithIRI: async(iri, headerOverrides) => {
         let endpoint = path.join(LRS_ENDPOINT, PATH_ACTIVITIES);
         let query = `?activityId=${encodeURIComponent(iri)}`;
 
@@ -86,6 +87,201 @@ const requests = {
         chai.expect(res.status).to.equal(200);
 
         return res.data;
+    },
+
+    /** 
+     * @typedef {Object} DocumentResponse
+     * @property {number} statusCode Status code of the LRS response.
+     * @property {Object} headers Response headers.
+     * @property {Object} data The object being returned.
+     */
+    /** 
+     * @typedef {Object} ActivityStateParameters
+     * @property {string} activityId The Activity id associated with this state.
+     * @property {Object} agent The Agent or Identified Group associated with this state.
+     * @property {string} registration The registration associated with this state.
+     * @property {string} stateId The id for this state, within the given context.
+     */
+    /**
+     * @typedef {ActivityStateParameters} ActivityStateGetParameters
+     * @property {string} since Additional Timestamp parameter for multiple document requests.  DO NOT USE FOR SINGLE DOCUMENTS.
+     */
+    /**
+     * PUT a document into the Activity State resource.
+     * @param {any} state Generic state document to store. 
+     * @param {ActivityStateParameters} params 
+     * @param {Object} headerOverrides Headers to override for this request. 
+     */
+    putState: async(state, params, headerOverrides) => {
+        return await requests.putDocument(PATH_ACTIVITIES_STATE, state, params, headerOverrides);
+    },
+
+    /**
+     * POST a document into the Activity State resource.
+     * @param {any} state Generic state document to store. 
+     * @param {ActivityStateParameters} params 
+     * @param {Object} headerOverrides Headers to override for this request. 
+     */
+    postState: async(state, params, headerOverrides) => {
+        return await requests.postDocument(PATH_ACTIVITIES_STATE, state, params, headerOverrides);
+    },
+
+    /**
+     * DELETE a document into the Activity State resource.
+     * @param {ActivityStateParameters} params 
+     * @param {Object} headerOverrides Headers to override for this request. 
+     */
+    deleteState: async(params, headerOverrides) => {
+        return await requests.deleteDocument(PATH_ACTIVITIES_STATE, params, headerOverrides);
+    },
+    
+    /**
+     * GET one or multiple documents from the Activity State resource.
+     * @param {ActivityStateParameters} params 
+     * @param {Object} headerOverrides Headers to override for this request. 
+     * @returns {DocumentResponse} The LRS's simplified response.
+     */
+    getSingleState: async(params, headerOverrides) => {
+        return await requests.getDocuments(PATH_ACTIVITIES_STATE, params, headerOverrides);
+    },
+    
+    /**
+     * GET one or multiple documents from the Activity State resource.
+     * @param {ActivityStateGetParameters} params 
+     * @param {Object} headerOverrides Headers to override for this request. 
+     */
+    getMultipleStates: async(params, headerOverrides) => {
+        return await requests.getDocuments(PATH_ACTIVITIES_STATE, params, headerOverrides);
+    },
+    
+    /** 
+     * @typedef {Object} AgentProfileParameters
+     * @property {string} profileId The Activity id associated with this state.
+     * @property {Object} agent The Agent or Identified Group associated with this state.
+     */
+    /**
+     * @typedef {Object} AgentProfileMultipleGetParameters
+     * @property {Object} agent The Agent or Identified Group associated with this state.
+     * @property {string} since Additional Timestamp parameter for multiple document requests.  DO NOT USE FOR SINGLE DOCUMENTS.
+     */
+    /**
+     * PUT a document into the Activity State resource.
+     * @param {any} document Generic state document to store. 
+     * @param {AgentProfileParameters} params 
+     * @param {Object} headerOverrides Headers to override for this request. 
+     */
+    putAgentProfile: async(document, params, headerOverrides) => {
+        return await requests.putDocument(PATH_AGENTS_PROFILE, document, params, headerOverrides);
+    },
+
+    /**
+     * POST a document into the Activity State resource.
+     * @param {any} document Generic state document to store. 
+     * @param {AgentProfileParameters} params 
+     * @param {Object} headerOverrides Headers to override for this request. 
+     */
+    postAgentProfile: async(document, params, headerOverrides) => {
+        return await requests.postDocument(PATH_AGENTS_PROFILE, document, params, headerOverrides);
+    },
+
+    /**
+     * DELETE a document into the Activity State resource.
+     * @param {ActivityStateParameters} params 
+     * @param {Object} headerOverrides Headers to override for this request. 
+     */
+    deleteAgentProfile: async(params, headerOverrides) => {
+        return await requests.deleteDocument(PATH_AGENTS_PROFILE, params, headerOverrides);
+    },
+    
+    /**
+     * GET one or multiple documents from the Activity State resource.
+     * @param {ActivityStateParameters} params 
+     * @param {Object} headerOverrides Headers to override for this request. 
+     * @returns {DocumentResponse} The LRS's simplified response.
+     */
+    getSingleAgentProfile: async(params, headerOverrides) => {
+        return await requests.getDocuments(PATH_AGENTS_PROFILE, params, headerOverrides);
+    },
+    
+    /**
+     * GET one or multiple documents from the Activity State resource.
+     * @param {AgentProfileMultipleGetParameters} params 
+     * @param {Object} headerOverrides Headers to override for this request. 
+     */
+    getMultipleAgentProfiles: async(params, headerOverrides) => {
+        return await requests.getDocuments(PATH_AGENTS_PROFILE, params, headerOverrides);
+    },
+    
+    /**
+     * Get a generic document from a generic resource path.
+     * @param {string} resourcePath Relative path to the resource endpoint, relative to the LRS's base xAPI path. 
+     * @param {Object} params Query parameters for the request.
+     * @param {Object} headerOverrides Optional headers to override for this request.
+     * @returns 
+     */
+    getDocuments: async(resourcePath, params, headerOverrides) => {
+        let endpoint = path.join(LRS_ENDPOINT, resourcePath);
+        let query = "?" + oldHelpers.getUrlEncoding(params);
+
+        let res = await axios.get(endpoint + query, {
+            headers: headerOverrides
+        });
+
+        return {
+            statusCode: res.status,
+            headers: res.headers,
+            data: res.data,
+        }
+    },
+    
+    /**
+     * PUT a generic document to a generic resource path.
+     * @param {string} resourcePath Relative path to the resource endpoint, relative to the LRS's base xAPI path. 
+     * @param {any} document Document to send.
+     * @param {Object} params Query parameters for the request.
+     * @param {Object} headerOverrides Optional headers to override for this request.
+     * @returns 
+     */
+    putDocument: async(resourcePath, document, params, headerOverrides) => {
+        let endpoint = path.join(LRS_ENDPOINT, resourcePath);
+        let query = "?" + oldHelpers.getUrlEncoding(params);
+
+        return await axios.put(endpoint + query, document, {
+            headers: headerOverrides
+        });
+    },
+    
+    /**
+     * POST a generic document to a generic resource path.
+     * @param {string} resourcePath Relative path to the resource endpoint, relative to the LRS's base xAPI path. 
+     * @param {any} document Document to send.
+     * @param {Object} params Query parameters for the request.
+     * @param {Object} headerOverrides Optional headers to override for this request.
+     * @returns 
+     */
+    postDocument: async(resourcePath, document, params, headerOverrides) => {
+        let endpoint = path.join(LRS_ENDPOINT, resourcePath);
+        let query = "?" + oldHelpers.getUrlEncoding(params);
+
+        return await axios.post(endpoint + query, document, {
+            headers: headerOverrides
+        });
+    },
+    
+    /**
+     * DELETE a generic document to a generic resource path.
+     * @param {string} resourcePath Relative path to the resource endpoint, relative to the LRS's base xAPI path. 
+     * @param {Object} params Query parameters for the request.
+     * @param {Object} headerOverrides Optional headers to override for this request.
+     * @returns 
+     */
+    deleteDocument: async(resourcePath, params, headerOverrides) => {
+        let endpoint = path.join(LRS_ENDPOINT, resourcePath);
+        let query = "?" + oldHelpers.getUrlEncoding(params);
+
+        return await axios.delete(endpoint + query, {
+            headers: headerOverrides
+        });
     }
 };
 
